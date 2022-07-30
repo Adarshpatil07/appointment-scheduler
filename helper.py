1cb1d7d4c774160d62a5e4b028f6867472f2f5c6
Adarshpatil07 <106315902+Adarshpatil07@users.noreply.github.com>
2022-07-30 14:06:21 +0530

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,18 @@
+def name_change(text):
+    if text == 'Medquest Diagnostics Center' or text == 'Medquest Diagnostics':
+        return 'Medquest Diagnostics Center'
+    elif text == 'Pronto Diagnostics' or text == 'Pronto Diagnostics Center':
+        return 'Pronto Diagnostics Center'
+    elif text == 'Vijaya Diagonstic Center' or text == 'Vijaya Diagnostic Center':
+        return 'Vijaya Diagnostic Center'
+    elif text == 'Viva Diagnostic' or text == 'Vivaa Diagnostic Center':
+        return 'Vivaa Diagnostic Center'
+    else:
+        return text
+
+import requests
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
